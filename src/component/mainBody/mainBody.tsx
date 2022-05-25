d36dab2e45749cac318ad01b74eccfb04c737c01
unknown <markothedevmail@gmail.com>
2022-05-25 22:17:04 +0100

    as of now the cart pop up does not show on mobile

--- src/component/mainBody/mainBody.tsx
+++ src/component/mainBody/mainBody.tsx
@@ -3,10 +3,19 @@ import ItemImagePreview from "./ItemImagePreview/ItemImagePreview"
 import {MainBodyContainer} from "./mainBody.style"
 import CustomModal from "../Modal/Modal"
 import { useState } from "react"
+import { useMediaQuery } from 'react-responsive'
+
 const MainBody = ()=>{
     const  [modalIsOpen,setModalIsOpen] = useState(false)
 
-    // 
+    
+    
+    const AvoidCartPopOnMobile = useMediaQuery({
+        query: '(min-width: 900px)'
+      })
+      console.log({
+        AvoidCartPopOnMobile
+      })
     return (
         <>        
         <br />
@@ -16,7 +25,12 @@ const MainBody = ()=>{
             element={<ItemImagePreview isInModalState={true}/>}
             />
         <MainBodyContainer>
-            <ItemImagePreview  openModalPic={()=>setModalIsOpen(true)} />
+            <ItemImagePreview  openModalPic={()=>{
+                if(AvoidCartPopOnMobile){
+                    //this means when it on mobile dont pop up use the arrow
+                    setModalIsOpen(true)
+                }
+                }} />
             <ItemDetail />
 
         </MainBodyContainer>
